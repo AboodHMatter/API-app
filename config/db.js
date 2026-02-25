151766f944da37eab1dbf43260072a6e0f4fcbfb
AboodMatter <engabdelrahmanmatter@gmail.com>
2026-02-25 12:13:57 +0200

    feat: Add initial MongoDB connection setup using Mongoose.

--- config/db.js
+++ config/db.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 const env = require('./env');
 
 const connectDB = async () => {
-    await mongoose.connect(env.MONGO_URI);
+    try {
+        await mongoose.connect(env.MONGO_URI);
+        console.log("MongoDB Connected!");
+    } catch (error) {
+        console.error("MongoDB Connection Error: ", error.message);
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
